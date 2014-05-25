0720ebe855d7c3b17e5b6a01f5466e9505f697e7
ZenLulz <zenlulz@binarysharp.com>
2014-05-25 16:50:11 +0200

    Update the header for 2014.

--- MemorySharp/Helpers/ArchitectureHelper.cs
+++ MemorySharp/Helpers/ArchitectureHelper.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * MemorySharp Library
+ * http://www.binarysharp.com/
+ *
+ * Copyright (C) 2012-2014 Jämes Ménétrey (a.k.a. ZenLulz).
+ * This library is released under the MIT License.
+ * See the file LICENSE for more information.
+*/
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using Binarysharp.MemoryManagement.Native;
--- MemorySharp/Memory/MemoryCore64.cs
+++ MemorySharp/Memory/MemoryCore64.cs
@@ -1,4 +1,12 @@
-﻿namespace Binarysharp.MemoryManagement.Memory
+﻿/*
+ * MemorySharp Library
+ * http://www.binarysharp.com/
+ *
+ * Copyright (C) 2012-2014 Jämes Ménétrey (a.k.a. ZenLulz).
+ * This library is released under the MIT License.
+ * See the file LICENSE for more information.
+*/
+namespace Binarysharp.MemoryManagement.Memory
 {
     /// <summary>
     /// Defines the memory interaction for 64-bit architecture.
